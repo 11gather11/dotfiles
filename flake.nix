f5a884d6f7302dca5e32978b10f88a0e0a2f1e3d
11gather11 <160300516+11gather11@users.noreply.github.com>
2026-02-01 17:55:53 +0900

    flake.nixからsystemsの依存関係を削除し、outputsにシステムを追加

--- flake.nix
+++ flake.nix
@@ -21,8 +21,6 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
-    systems.url = "github:nix-systems/default";
-
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
@@ -42,7 +40,6 @@
   outputs =
     inputs@{
       nixpkgs,
-      systems,
       flake-parts,
       treefmt-nix,
       git-hooks,
@@ -70,12 +67,16 @@
         };
     in
     flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "aarch64-darwin"
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+
       imports = [
         treefmt-nix.flakeModule
         git-hooks.flakeModule
       ];
-
-      systems = import systems;
 
       perSystem =
         {

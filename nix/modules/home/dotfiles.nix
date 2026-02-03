5d378dc658ddbc965c3a633bc137be1679c092f2
11gather11 <160300516+11gather11@users.noreply.github.com>
2026-02-03 23:55:45 +0900

    Zsh設定を追加: zshenvとzshrcファイルを作成し、シンボリックリンクを設定

--- nix/modules/home/dotfiles.nix
+++ nix/modules/home/dotfiles.nix
@@ -17,6 +17,12 @@ in
     # Fish shell configuration
     link_force "${dotfilesDir}/fish" "${configHome}/fish"
 
+    # Zsh environment
+    link_force "${dotfilesDir}/zshenv" "${homeDirectory}/.zshenv"
+
+    # Zsh configuration
+    link_force "${dotfilesDir}/zsh/zshrc" "${homeDirectory}/.zshrc"
+
     # Bash configuration
     link_force "${dotfilesDir}/bash/.bash_profile" "${homeDirectory}/.bash_profile"
     link_force "${dotfilesDir}/bash/.bashrc" "${homeDirectory}/.bashrc"
